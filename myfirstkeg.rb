831d1cca1bc0bb17f35c5cbbee4779875beff9db
John Harris <harris.johnny@gmail.com>
2016-08-14 23:56:49 -0600

    Update myfirstkeg.rb

--- myfirstkeg.rb
+++ myfirstkeg.rb
@@ -1,7 +1,7 @@
 class Myfirstkeg < Formula
   homepage "https://github.com/jharrys/myfirstkeg"
   url "https://github.com/jharrys/myfirstkeg/archive/1.0.0.tar.gz"
-  sha256 "4dfbf16bc3c8ee7395839bcc5c8f4729ddb4c80de8cf5b866088377e28a5b345"
+  sha256 ""
 
   def install
     bin.install "formalfriday"
